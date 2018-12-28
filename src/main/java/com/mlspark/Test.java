4dd86ee8ad016c0d461a38faec41de5d8bcdb54f
xiao.chen <bitxiao@hotmail.com>
2018-12-28 16:28:05 +0800

    项目初始化

--- src/main/java/com/mlspark/Test.java
+++ src/main/java/com/mlspark/Test.java
@@ -1,7 +1,7 @@
-import com.mlspark;
+package com.mlspark;
 
 public class Test {
-	public static void main(String[] args) {
-		System.out.println("test")
-	}
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
 }
